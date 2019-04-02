5ea5b2dc725fabb575c6cc0fd80abfef097a4abd
admin <admin@admins-MacBook-Pro.local>
2019-04-02 22:37:53 +0200

    tab Bar Custom

---
+++ MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by admin on 02/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+
+    }
+}
---
+++ MonRestoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by admin on 02/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
