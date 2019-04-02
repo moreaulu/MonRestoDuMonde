50c636c4bd0df675e3013f507612a354a5020ba5
admin <admin@admins-MacBook-Pro.local>
2019-04-02 22:18:47 +0200

    Ajout des Controllers principaux

---
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by admin on 02/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
