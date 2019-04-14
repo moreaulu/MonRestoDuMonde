bd64a83c09547851f20957d405135c95a3f45d6a
admin <admin@admins-MacBook-Pro.local>
2019-04-14 22:11:59 +0200

    ajout collectionView

--- MonRestoDuMonde/Controller/MenuController.swift
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class MenuController: UIViewController {
+class MenuController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let cellID = "MenuCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Les Menus Du Monde"
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? MenuCell {
+            cell.backgroundColor = GRIS_TRES_FONCE
+            return cell
+        }
+        return UICollectionViewCell()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        let largeur = collectionView.frame.width / 2 - 10
+        return CGSize(width: largeur, height: 200)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 25
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+        
     }
 }
+
---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by admin on 14/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
